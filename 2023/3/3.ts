84aab70fb86887922bcc34b54d1be98cf3926697
Tom Peng <pengtom2012@gmail.com>
2023-12-03 21:58:15 -0500

    2023.3.2

--- 2023/3/3.ts
+++ 2023/3/3.ts
@@ -1,30 +1,33 @@
 import { actual as input } from "./input";
 
 type NumberPosition = {
+  value: number;
   xStart: number;
   xEnd: number;
   y: number;
 };
 
-const checkBoundary = ({ xStart, xEnd, y }: NumberPosition) => {
+const gearLog: Record<number, Record<number, number[]>> = {};
+
+const checkBoundary = ({ xStart, xEnd, y, value }: NumberPosition) => {
   // start from the line above
   for (let i = xStart - 1; i <= xEnd + 1; i++) {
-    if (checkNodeIsSymbol(i, y - 1)) {
+    if (checkNodeIsSymbol(i, y - 1, value)) {
       return true;
     }
   }
 
   // current line
-  if (checkNodeIsSymbol(xStart - 1, y)) {
+  if (checkNodeIsSymbol(xStart - 1, y, value)) {
     return true;
   }
-  if (checkNodeIsSymbol(xEnd + 1, y)) {
+  if (checkNodeIsSymbol(xEnd + 1, y, value)) {
     return true;
   }
 
   // line beneath
   for (let i = xStart - 1; i <= xEnd + 1; i++) {
-    if (checkNodeIsSymbol(i, y + 1)) {
+    if (checkNodeIsSymbol(i, y + 1, value)) {
       return true;
     }
   }
@@ -32,13 +35,21 @@ const checkBoundary = ({ xStart, xEnd, y }: NumberPosition) => {
   return false;
 };
 
-const checkNodeIsSymbol = (x: number, y: number) => {
+const checkNodeIsSymbol = (x: number, y: number, value: number) => {
   // out of bound
   if (x < 0 || x > xSize - 1 || y < 0 || y > ySize - 1) {
     return false;
   }
-//   console.log(`checking line ${y} index ${x}: ${input[y][x]}`);
-  return !/^\d|\.$/.test(input[y][x]);
+  //   console.log(`checking line ${y} index ${x}: ${input[y][x]}`);
+  //   return !/^\d|\.$/.test(input[y][x]);
+
+  if (input[y][x] === "*") {
+    gearLog[x] ??= {};
+    gearLog[x][y] ??= [];
+    gearLog[x][y].push(value);
+  }
+
+  return false;
 };
 
 const xSize = input.length;
@@ -56,18 +67,29 @@ for (const [lineNum, line] of input.entries()) {
       const { 0: match, index } = res;
       const matchLen = match.length;
       const numPos: NumberPosition = {
+        value: Number(match),
         xStart: index,
         xEnd: index + matchLen - 1,
         y: lineNum,
       };
-    //   console.log(numPos, match)
-      const pass = checkBoundary(numPos);
-      if (pass) {
-        sum += Number(match);
-        // console.log({ lineNum, match });
-      }
+      //   console.log(numPos, match)
+      checkBoundary(numPos);
+      //   if (pass) {
+      //     sum += Number(match);
+      // console.log({ lineNum, match });
+      //   }
     }
   } while (res);
+}
+
+// console.log(gearLog);
+
+for (const [x, lineMap] of Object.entries(gearLog)) {
+  for (const [y, numbers] of Object.entries(lineMap)) {
+    if (numbers.length === 2) {
+      sum += numbers[0] * numbers[1];
+    }
+  }
 }
 
 console.log(sum);
